530cc8eda09830d0ecc9020d703930a3a0f67729
lukexiao <814008000@163.com>
2023-04-05 20:35:40 +0800

    1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,40 @@
 <script>
-    const adds=[
-        {name:'home',link:'/'},
-        {name:'one',link:'/one'},
-    ]
+
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger,DarkMode  } from 'flowbite-svelte'
+
+  import "../app.postcss";
+  const adds = [
+    { name: "home", link: "/" },
+    { name: "one", link: "/one" },
+  ];
+  
 </script>
-<main>
-    {#each adds as x}
-        <a href="{x.link}">{x.name}</a>
-        
-    {/each}
-    <hr>
-    <slot></slot>
-</main>
+
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <img
+      src="https://flowbite.com/docs/images/logo.svg"
+      class="mr-3 h-6 sm:h-9"
+      alt="Flowbite Logo"
+    />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      Flowbite
+    </span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/one">One</NavLi>
+    <NavLi href="/services">Services</NavLi>
+    <NavLi href="/pricing">Pricing</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+  <DarkMode></DarkMode>
+</Navbar>
+<div class="container mx-auto">
+  <slot></slot>
+</div>
+
+
+
 <style></style>
